a1f63749d498add1eee1f25d71c001115ef866df
HeyFajri <achmatfajri@gmail.com>
2023-09-10 23:56:10 -0800

    add props in button

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import Button from "./Button";
 const App = () => {
   return (
     <>
-      <div className="container flex justify-center items-center mx-auto w-full min-h-screen">
-        <Button />
-        <Button />
-        <Button />
-        <Button />
+      <div className="container flex justify-center items-center gap-x-5 mx-auto w-full min-h-screen">
+        <Button variant="bg-green-500">Login</Button>
+        <Button variant="bg-red-500">Logout</Button>
+        <Button variant="bg-slate-500">Register</Button>
       </div>
     </>
   );
--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,11 @@
-const Button = () => {
+const Button = ({ variant, children }) => {
   return (
     <>
       <button
-        className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+        className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`}
         type="submit"
       >
-        Buy now
+        {children}
       </button>
     </>
   );
